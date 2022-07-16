c41a756598a64f6763d7388dab1b7294cd29b3e6
Diego Paralon <diegoparalon@hotmail.com>
2022-07-16 22:15:07 +0100

    add sections and js structures

--- js/login.js
+++ js/login.js
@@ -1,2 +1,39 @@
-const inputEmail = document.querySelector('input-login')
-const inputPassword = document.querySelector('input-password')
+const inputEmail = document.querySelector('#input-login')
+const inputPassword = document.querySelector('#input-password')
+const btnRegistration = document.querySelector('.btn-registration')
+const inputText = document.querySelector('inputText')
+const spanError = document.querySelector('.span-error')
+const spanErrorPassword = document.querySelector('.span-error-password')
+
+
+
+
+
+
+btnRegistration.onclick = (e) => {
+    
+    if (inputEmail.value === '') {
+        inputEmail.classList.add('active')
+        spanError.classList.add('active')    
+        const preventDefault = e.preventDefault()   
+    } else {
+        inputEmail.classList.remove('active')
+        spanError.classList.remove('active') 
+    }
+
+    if (inputPassword.value === '') {
+        inputPassword.classList.add('active')
+        spanErrorPassword.classList.add('active') 
+        const preventDefault = e.preventDefault()
+    } else {
+        inputPassword.classList.remove('active')
+        spanErrorPassword.classList.remove('active') 
+
+    }
+
+   
+}
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 
-//FORM Codes
+//FORM Codes DOM 
 const inputBtnHome = document.querySelector(".btn-registration")
 const inputEmail = document.querySelector("#input-email")
 const spanError = document.querySelector(".span-error")
 
-//Language Codes 
+
+
+const questionOptLi = document.querySelectorAll('.questions-opt-frequent-close')
+const divAnswer = document.querySelectorAll('.content-answer-closed')
+
+
+
+
+
+//Language Codes DOM
 const liLanguage = document.querySelector(".li-language")
 
 
 //Function responsible for form verification 
 inputBtnHome.onclick = (e) => {
   let verify = false;
-  
+
   const valueEmail = inputEmail.value
 
   if (valueEmail === "") {
@@ -22,12 +31,76 @@ inputBtnHome.onclick = (e) => {
     spanError.classList.remove("active")
     alert('imagine aqui uma mensagem de reativaçao com sucesso! kk')
     let preventDefault = e.preventDefault()
-    
+
   }
 }
 
 // Language function translator 
 
 liLanguage.onclick = () => {
-    alert('Em breve')
+  alert('Em breve')
+
+}
+questionOptLi.onclick = () => {
+  for (let contador = 0; contador <= questionOptLi.length; contador++) {
+  
+    
+     
+    let verify = false
+      questionOptLi[contador].onclick = () => {
+        divAnswer[contador].classList.add('open')
+        verify = true
+      }
+  
+      if (verify === true) {
+        questionOptLi[contador].onclick = () => {
+          divAnswer[contador].classList.remove('open')
+      }
+  
+    } 
+  
+  
+  
+  
+  
+  
+
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
